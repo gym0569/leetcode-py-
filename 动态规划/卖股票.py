d506d3517751bac9c09862abdc11753e2cf5f6d0
gym0569 <44660202+gym0569@users.noreply.github.com>
2019-06-03 08:58:04 +0800

    Create 卖股票.py

---
+++ 动态规划/卖股票.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price,max_profit=float("inf"),0
+        for price in prices:
+            min_price=min(price,min_price)
+            max_profit=max(max_profit,price-min_price)
+        return max_profit
