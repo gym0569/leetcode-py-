371e13e876ae3e18de1b2775006bed016f4ed4f8
gym0569 <44660202+gym0569@users.noreply.github.com>
2019-06-03 09:25:23 +0800

    Create z字型遍历.py

---
+++ tree/z字型遍历.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if root is None:
+            return []
+        result,current,level=[],[root],1
+        while current:
+            next_level,vals=[],[]
+            for node in current:
+                vals.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            if level%2:
+                result.append(vals)
+            else:
+                result.append(vals[::-1])
+            level+=1
+            current=next_level
+        return result
